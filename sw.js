a6153cc75b204ff1070d901ed06cf2e840d6f211
Sebas <seplarui@gmail.com>
2021-03-31 22:55:15 +0200

    test changes

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/cache-list.js');
 
-var cacheName = 'chirpy-20210401.0447';
+var cacheName = 'chirpy-20210401.0454';
 
 function isExcluded(url) {
   const regex = /(^http(s)?|^\/)/; /* the regex for CORS url or relative url */
